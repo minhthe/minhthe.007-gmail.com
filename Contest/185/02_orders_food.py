3982dbce45aef40408d2240fb52e86c8868319a1
thetran <minhthe.007@gmail.com>
2020-04-20 14:17:22 +0000

    order foods

---
+++ Contest/185/02_orders_food.py
@@ -0,0 +1,40 @@
+'''
+https://leetcode.com/problems/display-table-of-food-orders-in-a-restaurant/
+
+'''
+class Solution:
+    def displayTable(self, orders: List[List[str]]) -> List[List[str]]:
+        
+        table = {}
+        dishes= []
+        
+        for order in orders:
+        
+            i, j , k = order[0], order[1], order[2]
+            
+            if j not in table:
+                table[j] = {}                   
+            
+            if k not in table[j]:
+                table[j][k] = 1
+            else:
+                table[j][k] += 1
+            
+            if k not in dishes:
+                dishes.append(k)
+                
+
+        dishes.sort()
+        rst = [['Table'] + (dishes)]
+        
+        for item in sorted(table.items(), key = lambda x: int(x[0])  ):
+            u, v = item[0], item[1]
+            tmp = [u]
+            for d in dishes:
+                if(d in v):
+                    tmp.append(str(v[d]))
+                else:
+                    tmp.append("0")
+            rst.append(tmp)
+        return rst
+            
