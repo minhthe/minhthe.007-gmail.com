abcf8a55e7aa7cbc7d2d1ca760c2118756b5bee2
thetran <minhthe.007@gmail.com>
2020-05-16 06:59:34 +0000

    pre in post Order of BST in recursion way: root.left + val + root.right -> in val + root.left + root.right -> pre root.left+ root.right + val -> post

---
+++ BST/print_PostOrder.py
@@ -0,0 +1,15 @@
+'''
+https://leetcode.com/problems/binary-tree-postorder-traversal/
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if root:
+            return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+        else:
+            return []
