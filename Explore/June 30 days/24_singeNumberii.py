c90517de306d8b2578c3eb92a0a999f7c77599a5
thetran <minhthe.007@gmail.com>
2020-06-22 07:31:48 +0000

    24/30 of june

---
+++ Explore/June 30 days/24_singeNumberii.py
@@ -0,0 +1,13 @@
+''''
+https://leetcode.com/problems/single-number-ii/
+'''
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        mp={}
+        for item in nums:
+            mp[item] = 1 if item not in mp else mp[item] + 1
+        for key in mp:
+            if mp[key] == 1 :
+                return key
+        
+            
