a7910655ad9c2820d9ddaf0f61bdd3bdd41bd099
thetran <minhthe.007@gmail.com>
2020-07-14 07:37:18 +0000

    pomodoro: substring with only 1

---
+++ PomodoroPractice/substrinwithone1.py
@@ -0,0 +1,6 @@
+'''
+https://leetcode.com/problems/number-of-substrings-with-only-1s/
+'''
+class Solution:
+	def numSub(self, s: str) -> int:
+		return sum([(len(item) *(len(item) + 1))//2 for item in s.split("0")]) % int(1e9  + 7)
