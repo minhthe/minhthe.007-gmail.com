d7a01aeb9df3e9f92f9aecb158eea7cd1a434e2f
thetran <minhthe.007@gmail.com>
2020-08-04 08:11:31 +0000

    pramp: linked list remove node

---
+++ pramp-condility-3month/linked-list-remove-val.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        dummy = ListNode(None)
+        dummy.next = head
+        currentNode = dummy
+        while(currentNode.next):
+            if currentNode.next.val == val:
+                currentNode.next =  currentNode.next.next
+            else:
+                currentNode = currentNode.next
+        return dummy.next
