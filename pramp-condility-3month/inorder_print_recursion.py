54d6d9f1f5e698349b3b960cb8c652991be90661
thetran <minhthe.007@gmail.com>
2020-09-07 03:57:19 +0000

    pranp: inroder traversal

---
+++ pramp-condility-3month/inorder_print_recursion.py
@@ -0,0 +1,5 @@
+''''https://leetcode.com/problems/binary-tree-inorder-traversal/'''
+class Solution:
+	def inorderTraversal(self, root: TreeNode) -> List[int]:
+		if root is None: return []
+		return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
